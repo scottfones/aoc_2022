ecb6ab2626271d85eb9724db899eefe9e7a4628c
Scott Fones <scott.fones@gmail.com>
2022-12-06 00:49:31 -0500

    day two done. added Outcome enum

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -43,6 +43,26 @@ impl FromStr for Hand {
     }
 }
 
+#[derive(Clone, Copy)]
+enum Outcome {
+    Win = 6,
+    Lose = 0,
+    Draw = 3,
+}
+
+impl FromStr for Outcome {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "X" => Ok(Outcome::Lose),
+            "Y" => Ok(Outcome::Draw),
+            "Z" => Ok(Outcome::Win),
+            _ => Err("Invalid Outcome type".to_string()),
+        }
+    }
+}
+
 fn main() {
     let t_start = Instant::now();
 
@@ -68,6 +88,37 @@ fn main() {
         })
         .sum();
     println!("Part One: {p1_score}");
+
+    let p2_score: u32 = input
+        //.par_lines()
+        .lines()
+        .map(|line| {
+            let playbook: (Hand, Outcome) = (
+                line.chars()
+                    .next()
+                    .map(|c| c.to_string().parse::<Hand>().unwrap())
+                    .unwrap(),
+                line.chars()
+                    .nth(2)
+                    .map(|c| c.to_string().parse::<Outcome>().unwrap())
+                    .unwrap(),
+            );
+
+            playbook.1 as u32
+                + match playbook {
+                    (Hand::Rock, Outcome::Win) => Hand::Paper as u32,
+                    (Hand::Rock, Outcome::Lose) => Hand::Scissors as u32,
+                    (Hand::Rock, Outcome::Draw) => Hand::Rock as u32,
+                    (Hand::Paper, Outcome::Win) => Hand::Scissors as u32,
+                    (Hand::Paper, Outcome::Lose) => Hand::Rock as u32,
+                    (Hand::Paper, Outcome::Draw) => Hand::Paper as u32,
+                    (Hand::Scissors, Outcome::Win) => Hand::Rock as u32,
+                    (Hand::Scissors, Outcome::Lose) => Hand::Paper as u32,
+                    (Hand::Scissors, Outcome::Draw) => Hand::Scissors as u32,
+                }
+        })
+        .sum();
+    println!("Part Two: {p2_score}");
 
     let t_total = t_start.elapsed().as_micros();
     println!("Day Two Time: {t_total} microseconds");
